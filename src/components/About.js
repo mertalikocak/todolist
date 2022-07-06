9fcdf1c41c6f4483ba1477ee051832c447636c53
Mert Ali Kocak <mertalikocakk@gmail.com>
2022-07-06 19:47:25 +0300

    finished

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h4>Github: mertalikocak</h4>
+      <a href="/">Go Back</a>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        onClick={onAdd}
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+      ></Button>
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+/* const headingStyle = { color: "red", backgroundColor: "black" }; */
+
+export default Header;
